5f44fc6a6fda3908f3109c664b2c651a6490cbfd
Paulo <youremail@yourdomain.com>
2023-03-12 21:36:00 -0700

    writes for loops to check items in an array and logs proper context

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,30 @@
-const courseList = {
-    code: ['ACIT 1515', 'ACIT 1620', 'ACIT 1630'],
-    name: ['Scripting For IT', 'Fundamental Web Technologies', 'Database Systems'],
-    program: 'CIT',
-}
+const courseList = [
+    {code: 'ACIT 1515', name: 'Scripting For IT'},
+    {code: 'ACIT 1620', name: 'Fundamental Web Technologies'},
+    {code: 'ACIT 1630', name: 'Database Systems'},
+]
 
 let user = "";
 do{
     user = prompt("Enter a 4 digit course code: ");
 }
 while(isNaN(user) || (user.length != 4));
-alert ("Hello")
+
+let found = false;
+for (let item of courseList) {
+    if (item.code.includes(user)) {
+        console.log(`Yes, I am taking the course: ${item.code} - ${item.name}`);
+        found = true;
+        break
+    }
+}
+if (found == false) {
+    for (let item of courseList) {
+        if (!item.code.includes(user)) {
+            const newCourse = {code: `ACIT ${user}`, name: null};
+            courseList.push(newCourse);
+            console.log(`Successfully added a new course ${newCourse.code}`);
+            break
+        }
+    }
+}
