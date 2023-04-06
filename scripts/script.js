83c75856eb38a45a71b2a654eb29d367433a9b43
Paulo <youremail@yourdomain.com>
2023-04-06 11:06:26 -0700

    checks if the array element contains the user number

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,33 @@
-const courseList = [
-    {code: 'ACIT 1515', name: 'Scripting For IT'},
-    {code: 'ACIT 1620', name: 'Fundamental Web Technologies'},
-    {code: 'ACIT 1630', name: 'Database Systems'},
-]
+// const courseList = [
+//     {code: 'ACIT 1515', name: 'Scripting For IT'},
+//     {code: 'ACIT 1620', name: 'Fundamental Web Technologies'},
+//     {code: 'ACIT 1630', name: 'Database Systems'},
+// ]
 
-let user = "";
-do{
-    user = prompt("Enter a 4 digit course code: ");
-}
-while(isNaN(user) || (user.length != 4));
+// let user = "";
+// do{
+//     user = prompt("Enter a 4 digit course code: ");
+// }
+// while(isNaN(user) || (user.length != 4));
 
-let found = false;
-for (let item of courseList) {
-    if (item.code.includes(user)) {
-        console.log(`Yes, I am taking the course: ${item.code} - ${item.name}`);
-        found = true;
-        break
-    }
-}
-if (found == false) {
-    for (let item of courseList) {
-        if (!item.code.includes(user)) {
-            const newCourse = {code: `ACIT ${user}`, name: null};
-            courseList.push(newCourse);
-            console.log(`Successfully added a new course ${newCourse.code}`);
-            break
-        }
-    }
-}
+// let found = false;
+// for (let item of courseList) {
+//     if (item.code.includes(user)) {
+//         console.log(`Yes, I am taking the course: ${item.code} - ${item.name}`);
+//         found = true;
+//         break
+//     }
+// }
+// if (found == false) {
+//     for (let item of courseList) {
+//         if (!item.code.includes(user)) {
+//             const newCourse = {code: `ACIT ${user}`, name: null};
+//             courseList.push(newCourse);
+//             console.log(`Successfully added a new course ${newCourse.code}`);
+//             break
+//         }
+//     }
+// }
 
 const courseDate = document.querySelector(".b-order")
 const courseCode = document.querySelectorAll(".a-order")
@@ -42,6 +42,11 @@ function createCourseArray() {
 
 const newCourseArray = createCourseArray();
 function findCourse(courseList) {
+    let user = "";
+    do{
+        user = prompt("Enter a 4 digit course code: ");
+    }
+    while(isNaN(user) || (user.length != 4));
     for (let item of courseList) {
         if (item.code.includes(user)) {
             console.log(courseCode)
