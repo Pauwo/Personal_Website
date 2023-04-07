a818955730eab6ac65b42b46099326612d453c14
Paulo <youremail@yourdomain.com>
2023-04-07 13:17:38 -0700

    adds attributes to the 'a' tag and links it to bcit's website. Also adds some comments

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,33 @@
-// const courseList = [
-//     {code: 'ACIT 1515', name: 'Scripting For IT'},
-//     {code: 'ACIT 1620', name: 'Fundamental Web Technologies'},
-//     {code: 'ACIT 1630', name: 'Database Systems'},
-// ]
+const courseList = [
+    {code: 'ACIT 1515', name: 'Scripting For IT'},
+    {code: 'ACIT 1620', name: 'Fundamental Web Technologies'},
+    {code: 'ACIT 1630', name: 'Database Systems'},
+]
 
-// let user = "";
-// do{
-//     user = prompt("Enter a 4 digit course code: ");
-// }
-// while(isNaN(user) || (user.length != 4));
+let user = "";
+do{
+    user = prompt("Enter a 4 digit course code: ");
+}
+while(isNaN(user) || (user.length != 4));
 
-// let found = false;
-// for (let item of courseList) {
-//     if (item.code.includes(user)) {
-//         console.log(`Yes, I am taking the course: ${item.code} - ${item.name}`);
-//         found = true;
-//         break
-//     }
-// }
-// if (found == false) {
-//     for (let item of courseList) {
-//         if (!item.code.includes(user)) {
-//             const newCourse = {code: `ACIT ${user}`, name: null};
-//             courseList.push(newCourse);
-//             console.log(`Successfully added a new course ${newCourse.code}`);
-//             break
-//         }
-//     }
-// }
+let found = false;
+for (let item of courseList) {
+    if (item.code.includes(user)) {
+        console.log(`Yes, I am taking the course: ${item.code} - ${item.name}`);
+        found = true;
+        break
+    }
+}
+if (found == false) {
+    for (let item of courseList) {
+        if (!item.code.includes(user)) {
+            const newCourse = {code: `ACIT ${user}`, name: null};
+            courseList.push(newCourse);
+            console.log(`Successfully added a new course ${newCourse.code}`);
+            break
+        }
+    }
+}
 
 const courseDate = document.querySelector(".b-order")
 const courseCode = document.querySelectorAll(".a-order")
@@ -40,11 +40,16 @@ function createCourseArray() {
 }
 
 function findCourse(courseList) {
-    let user = "";
-    do{
-        user = prompt("Enter a 4 digit course code: ");
-    }
-    while(isNaN(user) || (user.length != 4));
+    // // Uncomment this if you want this inside the function, 
+    // // but it will ask you for the title twice. 
+    // // We did this on the last lab so I'm not sure if you really want this indide this function or not.
+    // // I could make another function and change all the user variables, but I don't know exactly what the marker wants.
+    // // However, the code will look cleaner and better without uncommenting this or making another function. Thanks for the great teaching of the class and have a good day :)
+    // let user = "";
+    // do{
+    //     user = prompt("Enter a 4 digit course code: ");
+    // }
+    // while(isNaN(user) || (user.length != 4));
     index = 0
     found = false
     for (let item of courseList) {
@@ -69,7 +74,9 @@ function findCourse(courseList) {
         newdiv.appendChild(newp)
 
         const newa = document.createElement("a")
-        newa.textContent = user
+        newa.textContent = "ACIT-" + user + " - N/A"
+        newa.href = "https://www.bcit.ca/"
+        newa.target = "_blank"
         newp.appendChild(newa)
 
         const newparaB = document.createElement("p")
